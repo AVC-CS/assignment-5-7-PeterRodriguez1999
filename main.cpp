1514a31c9f7966890dd76485179a7936ae4fa81a
PeterRodriguez1999 <156959793+PeterRodriguez1999@users.noreply.github.com>
2024-03-13 16:11:26 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,14 @@ int main()
 	cin >> n1 >> n2;
 	cin >> N;
 
-	/***************************************************
-	 * Code your program here
-	 **************************************************	*/
+	cout << n1  << " " << n2 << " " ;
+	for ( int i = 2; i < N; ++i) {
+		int sum = n1 + n2;
+		cout << sum << " ";
+		n1 = n2;
+		n2 =sum;
+	}
+	cout << endl;
+
+	return 0;
 }
